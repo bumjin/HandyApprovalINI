a7029c2436a018f3d7a8d411ca1e292276ee9c71
bumjin <bumjin@gmail.com>
2021-08-19 17:33:29 +0900

    UPDATE: show approval ini

--- api/HttpTrigger/index.js
+++ api/HttpTrigger/index.js
@@ -1,15 +1,62 @@
+//const xlsx = require('xlsx')
+const Excel = require('exceljs');
+//const iconv = require('iconv-lite'); //한글깨짐방지
+
 module.exports = async function (context, req) {
+    
+    // @files 엑셀 파일을 가져온다.
+
     context.log('JavaScript HTTP trigger function processed a request.');
-    var utcDate = new Date();
+    // read from a file
+    const workbook = new Excel.Workbook();
+    await workbook.xlsx.readFile("HANDY_HSO_Approval_ini_20210531.xlsx");
+    let raw = [];
+        workbook.eachSheet((sheet, sid)=> {
+            //console.log('sheet name=' + sheet.name);
+            if(sheet.name != 'globals.properties') {
+                return;
+            }
+            sheet.eachRow((row, rid)=> {
+                if(rid < 4) {
+                    return;
+                }
+                
+                //4행 옵션명, 설정값, 설명, Default, Version, 비고
+                let optionname = '';
+                let optionvalue = '';
+                let optiondescription = '';
+                let optiondefault = '';
+                let optionversion = '';
+                let optionetc = '';
+                row.eachCell((cell, cid)=> {
+                    if(cid == 1) {
+                        optionname = cell.value
+                    } else if(cid == 2) {
+                        optionvalue = cell.value
+                    } else if(cid == 3) {
+                        optiondescription = cell.value
+                    } else if(cid == 4) {
+                        optiondefault = cell.value
+                    } else if(cid == 5) {
+                        optionversion = cell.value
+                    } else if(cid == 6) {
+                        optionetc = cell.value
+                    }
+                    //console.log('cid='+cid+' cell.value=' + cell.value)
+                })
+                raw.push({
+                    'optionname':optionname, 'optionvalue':optionvalue, 'optiondescription':optiondescription,
+                    'optiondefault':optiondefault, 'optionversion':optionversion, 'optionetc':optionetc
+                });
+            })
+         })
+      
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         headers: {"Access-Control-Allow-Origin": "*"},
-        body: {message: utcDate.toISOString()}
-        //res time
+        body: {data: raw}
+        //res ini
     };
 }
